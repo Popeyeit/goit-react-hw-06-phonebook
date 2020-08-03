5bab7f3587358237854ace1ce6cf1045bdc18404
PopeyeIT <igorserov28@gmail.com>
2020-08-03 15:50:28 +0300

    Updates

---
+++ precache-manifest.1d7c2a0db8d30ea56144628c2ff070ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c26274c0cf8069c38440cfc0e59e3b6e",
+    "url": "/goit-react-hw-05-phonebook/index.html"
+  },
+  {
+    "revision": "da9b57f31c5ac10390ce",
+    "url": "/goit-react-hw-05-phonebook/static/css/main.f414bcdd.chunk.css"
+  },
+  {
+    "revision": "94e23ec0213dd1e366f6",
+    "url": "/goit-react-hw-05-phonebook/static/js/2.7542a538.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-05-phonebook/static/js/2.7542a538.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da9b57f31c5ac10390ce",
+    "url": "/goit-react-hw-05-phonebook/static/js/main.53038015.chunk.js"
+  },
+  {
+    "revision": "3f441844d3ba7dba1f3a",
+    "url": "/goit-react-hw-05-phonebook/static/js/runtime-main.61bf2916.js"
+  }
+]);
